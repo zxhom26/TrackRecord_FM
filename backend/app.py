850f42c2c9084d7cae2c1aecc3dd858735dbd61c
zxhom26 <125313839+zxhom26@users.noreply.github.com>
2025-10-30 14:10:50 -0700

    TRF-33 testing backend connection

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from spotify_api import SpotifyAPI, SpotifyAPIProxy
+
+user_tokens = {} # temp token storage for dev
 
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["track-record-fm-test.vercel.app"],  # your Next.js frontend
+    allow_origins=["track-record-fm-test.vercel.app",
+                    "http://localhost:3000"],  # your Next.js frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -13,15 +16,26 @@ app.add_middleware(
 
 @app.post("/api/token")
 async def get_token(request: Request):
+    """
+    Receives access token from front end and stores it for api calls
+    """
     data = await request.json()
     token = data.get('accessToken')
-    return token 
+    if not token:
+        return {"error": "token not found"}
+    user_tokens['active'] = token
+    return {"error": "token not found"}
 
-@app.get("/api/data")
+@app.get("/api/data") # need to specify diff endpoints
 def get_data():
+    # token = user_tokens['active']
+    # if not token:
+    #     return {"error": "token not found. cannot make api calls"}
 
-    api = SpotifyAPI() # client id param
-    proxy = SpotifyAPIProxy(api, token)
+    # api = SpotifyAPI()
+    # proxy = SpotifyAPIProxy(api, token)
+
+    # data = proxy.fetch_api(endpoint="me/top/tracks") # default method GET
 
     return {"message": "Hello from Python backend!"}
 
@@ -30,5 +44,4 @@ top artists
 top songs
 minutes (daily, weekly, monthly)
 genre
-
 '''
