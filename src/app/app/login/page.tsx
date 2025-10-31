d997cd6c5dce5d66ea298fe8507d1075b57898ba
dishaamin4 <87672658+dishaamin4@users.noreply.github.com>
2025-10-31 16:43:33 -0500

    Create page.tsx

---
+++ src/app/app/login/page.tsx
@@ -0,0 +1,14 @@
+'use client'; // marking as a client component
+
+import AuthButton from '../components/AuthButton'; //importing our AuthButton component -- separating logic and UI
+//import Link from 'next/link';
+import '../login.css'; // import css for styling 
+
+export default function LoginPage() { // React functional component for login page
+  return (
+    <main className="login-page"> {/* login page styling */}
+      <h1 className="login-title">Welcome to TrackRecord FM</h1> {/* welcome text styling */}
+      <AuthButton /> {/* linking to spotify login button */}
+    </main>
+  );
+}
