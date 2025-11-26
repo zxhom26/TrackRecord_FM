4e1ac9da3b8237584f792c359aa51c5dd22ca5a3
dishaamin4 <87672658+dishaamin4@users.noreply.github.com>
2025-11-26 14:41:08 -0600

    editing endpoint name

--- src/utils.js
+++ src/utils.js
@@ -72,7 +72,7 @@ export async function fetchTopTracks(token) {
 export async function fetchDiscoverWeekly(token) {
   try {
     const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/spotify-discover`,
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/spotify-weekly`,
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
